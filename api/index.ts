510da67509797773469aab9fe8923c25e741d787
Justin San Juan <justin.sj@alumni.ust.hk>
2021-08-21 19:55:50 -0400

    refactor: articles renderer

--- api/index.ts
+++ api/index.ts
@@ -3,56 +3,12 @@ import config from 'config';
 import staticResponse from 'config/static.json';
 import { Article, Video } from 'types';
 import { async, getEnv, parseYouTubeResponse } from 'utils';
-
 export async function getArticles(): Promise<Article[]> {
-  const showArticles = Boolean(config.articles);
-  const articles: Article[] = [];
-
-  if (!showArticles) {
-    return articles;
-  }
-
-  const url: string = `${config.articles?.apiUrl}/articles`;
-  const request: AxiosRequestConfig = {
-    params: {
-      username: config.articles?.username,
-    },
-  };
-
-  const [response, error] = await async<AxiosResponse, AxiosError>(
-    axios.get(url, request),
-  );
-
-  if (error) {
-    const message = error?.response?.data?.error?.message;
-    console.log(message);
-    return articles;
-  }
-
-  response?.data.forEach((data: any) => {
-    const {
-      id,
-      title,
-      description,
-      tag_list: tags,
-      published_at: publishedAt,
-      url,
-    } = data;
-
-    const article: Article = {
-      id,
-      title,
-      description,
-      url,
-      tags,
-      publishedAt,
-    };
-    articles.push(article);
-  });
-
-  return articles;
+  // Delegation to renderer object (Strategy Pattern)
+  return config.articles
+    ? config.articles.renderer.getArticles()
+    : [];
 }
-
 export async function getVideos(): Promise<Video[]> {
   const showVideos = Boolean(config.videos);
   const videos: Video[] = [];
--- config/articles.ts
+++ config/articles.ts
@@ -1,17 +1,84 @@
-export enum ArticleProvider {
-  DEVTO = 'dev.to',
+import { Article } from '../types';
+import config from './index';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { async } from '../utils';
+
+/**
+ * Base class for rendering articles
+ */
+export class ArticlesRenderer {
+  /**
+   * Returns Article[]
+   */
+  async getArticles(): Promise<Article[]> {
+    return [];
+  }
 }
 
-export type DevtoConfig = {
-  provider: ArticleProvider.DEVTO;
-  username: string;
-  apiUrl: string;
+export class DevToArticlesRenderer {
+  private username: string;
+  private apiUrl: string;
+
+  constructor(username: string, apiUrl: string){
+    this.username = username;
+    this.apiUrl = apiUrl;
+  }
+  async getArticles(): Promise<Article[]> {
+    if (this.apiUrl == null) return [];
+    if (this.username == null) return [];
+
+    const articles: Article[] = [];
+
+    const url: string = `${this.apiUrl}`;
+    const request: AxiosRequestConfig = {
+      params: {
+        username: this.username,
+      },
+    };
+
+    const [response, error] = await async<AxiosResponse, AxiosError>(
+      axios.get(url, request),
+    );
+
+    if (error) {
+      const message = error?.response?.data?.error?.message;
+      console.log(message);
+      return articles;
+    }
+
+    response?.data.forEach((data: any) => {
+      const {
+        id,
+        title,
+        description,
+        tag_list: tags,
+        published_at: publishedAt,
+        url,
+      } = data;
+
+      const article: Article = {
+        id,
+        title,
+        description,
+        url,
+        tags,
+        publishedAt,
+      };
+      articles.push(article);
+    });
+
+    return articles;
+  }
+}
+export type ArticlesConfig = {
+  renderer: ArticlesRenderer,
 };
 
-export type Articles = DevtoConfig;
+export type Articles = ArticlesConfig;
 
-export const articles: DevtoConfig = {
-  provider: ArticleProvider.DEVTO,
-  username: 'karanpratapsingh',
-  apiUrl: 'https://dev.to/api',
+export const articles: ArticlesConfig = {
+  renderer: new DevToArticlesRenderer(
+    'karanpratapsingh',
+    'https://dev.to/api/articles'
+  ),
 };
